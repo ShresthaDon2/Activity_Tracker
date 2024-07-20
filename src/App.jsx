142e8b5bcf049f8b24c3572dcad90ffe6f78f790
Ishan Shrestha <ishan.stha360@gmail.com>
2024-07-20 12:44:24 +0545

    Footer Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <>Hello World</>;
-}
+import Footer from "./layouts/Footer";
+import Home from "./pages/Home";
+const App = () => {
+  return (
+    <>
+      <div className="container vh-90">
+        <div style={{ minHeight: "300px" }}>
+          <Home />
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+};
 
 export default App;
